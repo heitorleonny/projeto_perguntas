af829a01fda6d4f258d84e8cad42c64ddbba7fca
heitorleonny <heitorleonnyyt1234@gmail.com>
2023-11-06 23:55:54 -0300

    add score system

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,46 @@
 import 'package:flutter/material.dart';
-import './questao.dart';
-import './resposta.dart';
+import './questionario.dart';
+import './resultado.dart';
 
 main() => runApp(PerguntaApp());
 
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
-  final  List<Map<String, Object>> _perguntas = [
-      {
-        'texto': 'Qual é a sua cor favorita?',
-        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
-      },
-      {
-        'texto': 'Qual é o seu animal favorito?',
-        'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
-      },
-      {
-        'texto': 'Qual o seu instrutor favorito?',
-        'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
-      },
-    ];
-  
+  var _pontuacaoTotal = 0;
+  final List<Map<String, Object>> _perguntas = [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': [
+        {'texto': 'Preto', 'pontuacao': 10},
+        {'texto': 'Vermelho', 'pontuacao': 5},
+        {'texto': 'Verde', 'pontuacao': 3},
+        {'texto': 'Branco', 'pontuacao': 1},
+      ],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': [
+        {'texto': 'Coelho', 'pontuacao': 10},
+        {'texto': 'Cobra', 'pontuacao': 5},
+        {'texto': 'Elefante', 'pontuacao': 3},
+        {'texto': 'Leão', 'pontuacao': 1},
+      ],
+    },
+    {
+      'texto': 'Qual o seu instrutor favorito?',
+      'respostas': [
+        {'texto': 'Maria', 'pontuacao': 10},
+        {'texto': 'João', 'pontuacao': 5},
+        {'texto': 'Leo', 'pontuacao': 3},
+        {'texto': 'Pedro', 'pontuacao': 1},
+      ],
+    },
+  ];
 
-  void _responder() {
+  void _responder(int pontuacao) {
     setState(() {
       _perguntaSelecionada++;
+      _pontuacaoTotal += pontuacao;
     });
   }
 
@@ -33,31 +49,18 @@ class _PerguntaAppState extends State<PerguntaApp> {
   }
 
   Widget build(BuildContext context) {
-    
-    List<String> respostas = temPerguntaSelecionada? _perguntas[_perguntaSelecionada].cast()['respostas']: [];
-  List<Widget> widgets = respostas.map((t) => Resposta(t, _responder)).toList();
-
-    //for(var textoResp in respostas){
-     //respostas.add(Resposta(textoResp, _responder));
-    //}
-
     return MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Perguntas'),
-    ),
-    body: temPerguntaSelecionada
-        ? Column(
-            children: <Widget>[
-              Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
-              ...respostas.map((t) => Resposta(t, _responder)).toList(),
-            ],
-          )
-        : Center(
-            child: Text("Não há mais perguntas."),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Perguntas'),
           ),
-  ),
-);
+          body: temPerguntaSelecionada
+              ? Questionario(
+                  perguntas: _perguntas,
+                  perguntaSelecionada: _perguntaSelecionada,
+                  quandoResponder: _responder)
+              : Resultado(_pontuacaoTotal)),
+    );
   }
 }
 
---
+++ lib/questionario.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import './questao.dart';
+import './resposta.dart';
+
+class Questionario extends StatelessWidget {
+  final int perguntaSelecionada;
+  final List<Map<String, Object>> perguntas;
+  final void Function(int) quandoResponder;
+
+  Questionario(
+      {required this.perguntas,
+      required this.perguntaSelecionada,
+      required this.quandoResponder});
+
+  bool get temPerguntaSelecionada {
+    return perguntaSelecionada < perguntas.length;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Map<String, Object>> respostas = temPerguntaSelecionada
+        ? perguntas[perguntaSelecionada].cast()['respostas']
+        : [];
+
+    return Column(
+      children: <Widget>[
+        Questao(perguntas[perguntaSelecionada]['texto'].toString()),
+        ...respostas
+            .map((resp) => Resposta(
+                resp['texto'] as String,() => quandoResponder(int.parse(resp['pontuacao'].toString()))))
+            .toList(),
+      ],
+    );
+  }
+}
